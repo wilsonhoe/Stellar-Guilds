2e1d1b64f15e9e73c72d450a00923f8fe9ffe259
gabito1451 <obiagaziegabriel@gmail.com>
2026-04-01 01:48:38 -0700

    chore: push all ts and tsx file changes

--- backend/src/guilds/dto/create-guild.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateGuildDto {
-  @ApiProperty({
-    example: 'My Guild',
-    description: 'Name of the guild',
-  })
-  name: string;
-}
--- backend/src/users/test/mocks/prisma.mock.ts
+++
@@ -1,5 +0,0 @@
-import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
-import { PrismaClient } from '@prisma/client';
-
-export const prismaMock: DeepMockProxy<PrismaClient> =
-  mockDeep<PrismaClient>();
